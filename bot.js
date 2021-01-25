9a375aa5b0c5c4187fc09e2483e4c2f583e029c1
Vincent Beugnet <beugnet.vincent@outlook.com>
2021-01-25 14:42:32 +0100

    First tests

---
+++ bot.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+const config = require("./config.json");
+
+const power = require("./commands/power")
+const list = require("./commands/list")
+
+const client = new Discord.Client();
+client.once('ready', () => {
+    console.log(`Il est maintenant l'heure du ${client.user.tag}!`);
+});
+
+
+client.on("message", function(message) {
+    if (!message.author.bot) {
+        const command = message.content.toLowerCase();
+
+        if (command === "be power") {
+            return power(message)
+        }
+        else if (command === "be liste") {
+            return list(message)
+        }
+    }
+});
+
+
+client.login(config.BOT_TOKEN).then();
